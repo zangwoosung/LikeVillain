28230d658a277111e19c44eb8dc26abde599ed20
zangwoosung <54176171+zangwoosung@users.noreply.github.com>
2025-05-20 01:09:19 +0900

    add jump.cs

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Set isGrounded true when touching the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
