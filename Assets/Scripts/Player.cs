b7e080828a326693b056610530ae193c8b810ca4
zangwoosung <54176171+zangwoosung@users.noreply.github.com>
2025-05-20 00:52:13 +0900

    [import]  char

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,15 +3,36 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public Animator anim;
+    public float jumpForce = 5f;
+    private Rigidbody rb;
+    private bool isGrounded;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
+    } 
+    public float speed = 5f;
+    void OnCollisionEnter(Collision collision)
+    {
+        // Set isGrounded true when touching the ground
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        float moveInput = Input.GetAxis("Vertical"); // W/S or Up/Down Arrow by default
+        transform.Translate(Vector3.forward * moveInput * speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
         if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("A");
@@ -26,6 +47,7 @@ public class Player : MonoBehaviour
             anim.SetTrigger("Backward");
 
         }
+      
         if (Input.GetKeyDown(KeyCode.R))
         {
             Debug.Log("R");
