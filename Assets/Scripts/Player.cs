b9b195c2f426b504f0f8a082320c742c630d9699
zangwoosung <54176171+zangwoosung@users.noreply.github.com>
2025-05-26 00:43:56 +0900

    [fix] add tag 'ground'

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,56 +4,79 @@ public class Player : MonoBehaviour
 {
     public Animator anim;
     public float jumpForce = 5f;
+    public float moveSpeed = 5;
+    public float speed = 5f;
+    public float bounceForce = 10f;
+
     private Rigidbody rb;
     private bool isGrounded;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Quaternion originalRotation;
+    
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     } 
-    public float speed = 5f;
+  
+    
+    void StandUp()
+    {
+        transform.rotation= originalRotation;
+    }
     void OnCollisionEnter(Collision collision)
     {
-        // Set isGrounded true when touching the ground
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            Vector3 collisionNormal = collision.contacts[0].normal;
+            rb.AddForce(collisionNormal * bounceForce, ForceMode.Impulse);
+            Invoke("StandUp", 2);           
+        }
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
         }
-    }
-
-    // Update is called once per frame
+    }    
     void Update()
     {
-        float hmoveInput = Input.GetAxis("Horizontal"); // W/S or Up/Down Arrow by default
-        transform.Translate(Vector3.forward * hmoveInput * speed * Time.deltaTime);
-
-
-
-        float moveInput = Input.GetAxis("Vertical"); // W/S or Up/Down Arrow by default
-        transform.Translate(Vector3.forward * moveInput * speed * Time.deltaTime);
-       
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.K))
         {
-            Debug.Log("A");
-
-            anim.SetTrigger("DoubleJump");
-
-        }
-        if (Input.GetKeyDown(KeyCode.V))
-        {
-            Debug.Log("V");
-
-            anim.SetTrigger("AirSpin");
-
-        }
-      
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            Debug.Log("R");
-
-            anim.SetTrigger("Run");
-
+            anim.SetTrigger("AirSpine");
         }
 
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+        // Get input for horizontal movement
+        float horizontalInput = 0;
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            horizontalInput -= 1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            horizontalInput += 1;
+        }
+
+        // Get input for vertical movement
+        float verticalInput = 0;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            verticalInput += 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            verticalInput -= 1;
+        }
+
+        // Calculate the movement vector
+        Vector3 movement = new Vector3(horizontalInput, 0, verticalInput);
+
+        // Normalize the vector to ensure consistent movement speed
+        movement.Normalize();
+
+        // Move the object
+        transform.Translate(movement * moveSpeed * Time.deltaTime);
     }
 }
