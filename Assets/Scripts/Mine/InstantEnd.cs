fef5bad668f92a5164d06aa3522fead9119a5bd6
zangwoosung <54176171+zangwoosung@users.noreply.github.com>
2025-06-12 00:16:15 +0900

    [fix] bastItem

--- Assets/Scripts/Mine/InstantEnd.cs
+++ Assets/Scripts/Mine/InstantEnd.cs
@@ -1,15 +1,34 @@
-﻿using UnityEngine;
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
 
-public class InstantEnd : Mine
+
+public class BaseItem : MonoBehaviour
 {
-    public override void OnTriggerEnter(Collider other)
+    //test change
+    public static event Action<AudioType> OnHitEvent;
+    public AudioType myType;
+    int damage = 10;
+    public PlayerData playerData;
+    public virtual void OnTriggerEnter(Collider other)
     {
+        Debug.Log("tyep " +myType.ToString());
+        OnHitEvent?.Invoke(myType);
+    }
+
+
+}
+public class InstantEnd : BaseItem
+{   
+    public new void OnTriggerEnter(Collider other) 
+    {
+        base.OnTriggerEnter(other);
+        
         if (other.CompareTag("Player"))
         {
             playerData.HP = 0;
             playerData.Life = 0;
             playerData.CalculateHP();
-            base.OnTriggerEnter(other);
 
         }
     }
